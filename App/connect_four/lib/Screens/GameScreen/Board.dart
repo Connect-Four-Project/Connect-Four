1424b349be40c9621dad360e09eb7d79d89a46eb
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-21 03:22:41 +0200

    Enhanced board style

--- App/connect_four/lib/Screens/GameScreen/Board.dart
+++ App/connect_four/lib/Screens/GameScreen/Board.dart
@@ -15,12 +15,39 @@ List<BoardColumn> buildBoard() {
 class Board extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(30 , 120 , 30 , 30),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: buildBoard(),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+            color: Colors.blue,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              )
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: buildBoard(),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
