1a1f6a0c9e12e7ee7d2613ad31cc928681787ad7
Omar AboSrea <omar22mohamed5@gmail.com>
2021-04-26 00:16:29 +0200

    Changed board animation from explicit to implicit animation

--- App/connect_four/lib/screens/game_screen/board.dart
+++ App/connect_four/lib/screens/game_screen/board.dart
@@ -16,27 +16,9 @@ class Board extends StatefulWidget {
 }
 
 class _BoardState extends State<Board> with SingleTickerProviderStateMixin {
-  AnimationController _animationController;
-  Animation _colorTween;
-  Animation _shadowTween;
-
   @override
   initState() {
     super.initState();
-
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
-    _colorTween = ColorTween(begin: Colors.yellow, end: Colors.red)
-        .animate(_animationController);
-    _shadowTween =
-        ColorTween(begin: Colors.amber, end: Constants.PrimaryDarkColor)
-            .animate(_animationController);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _animationController.dispose();
   }
 
   List<Widget> _buildBoard() {
@@ -47,10 +29,6 @@ class _BoardState extends State<Board> with SingleTickerProviderStateMixin {
           onTap: () {
             setState(() {
               Controller.getInstance().playColumn(j);
-              if (Controller.getInstance().isPlayerOneTurn())
-                _animationController.reverse();
-              else
-                _animationController.forward();
             });
           },
           child: BoardColumn(
@@ -63,19 +41,28 @@ class _BoardState extends State<Board> with SingleTickerProviderStateMixin {
   }
 
   Widget getText(Size size) {
-    return Text(
-      Controller.getInstance().isPlayerOneTurn() ? 'PLAYER ONE' : 'PLAYER TWO',
+    return AnimatedDefaultTextStyle(
+      duration: Duration(milliseconds: 500),
       style: TextStyle(
-        color: _colorTween.value,
+        color: Controller.getInstance().isPlayerOneTurn()
+            ? Colors.yellow
+            : Colors.red,
         shadows: [
           Shadow(
-            color: _shadowTween.value,
+            color: Controller.getInstance().isPlayerOneTurn()
+                ? Colors.amber
+                : Constants.PrimaryDarkColor,
             blurRadius: 2.0,
             offset: Offset(-3.0, 2.0),
           ),
         ],
         fontFamily: 'PressStart2P',
-        fontSize: size.width * 0.075,
+        fontSize: size.width * 0.045,
+      ),
+      child: Text(
+        Controller.getInstance().isPlayerOneTurn()
+            ? 'PLAYER ONE'
+            : 'PLAYER TWO',
       ),
     );
   }
@@ -83,48 +70,43 @@ class _BoardState extends State<Board> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return AnimatedBuilder(
-      animation: _animationController,
-      builder: (BuildContext context, _) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            getText(size),
-            Padding(
-              padding: const EdgeInsets.only(top: 90),
-              child: Container(
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                  color: Colors.blue[800],
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.7),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    )
-                  ],
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: _buildBoard(),
-                    ),
-                  ],
-                ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        getText(size),
+        Padding(
+          padding: const EdgeInsets.only(top: 90),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
               ),
+              color: Colors.blue[800],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.7),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                )
+              ],
             ),
-          ],
-        );
-      },
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: _buildBoard(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
