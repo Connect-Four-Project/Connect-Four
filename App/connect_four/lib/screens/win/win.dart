c50a201d35edfdef44f362deadbb264372eee913
Reham Tarek <rehamtarek666666@gmail.com>
2021-04-29 07:31:33 +0200

    added animated text

--- App/connect_four/lib/screens/win/win.dart
+++ App/connect_four/lib/screens/win/win.dart
@@ -1,3 +1,8 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:connect_four/components/rounded_button.dart';
+import 'package:connect_four/constants/constants.dart';
+import 'package:connect_four/screens/welcome/welcome_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class win extends StatelessWidget {
@@ -8,8 +13,41 @@ class win extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        message,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DefaultTextStyle(
+              style: const TextStyle(fontSize: 60.0, fontFamily: "Nunito"),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  WavyAnimatedText(message),
+                ],
+                isRepeatingAnimation: true,
+                repeatForever: true,
+              ),
+            ),
+
+            RoundedButton(
+              text: "PLAY AGAIN ",
+              press: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WelcomeScreen();
+                    },
+                  ),
+                );
+              },
+              icon: Icon(
+                Icons.keyboard_return_rounded,
+                color: Constants.PrimaryLightColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
