4b95f572ad8b197ccf7734ade0f05ec6a379e4eb
Reham Tarek <rehamtarek666666@gmail.com>
2021-05-03 07:44:05 +0200

    added board to finish screen

--- App/connect_four/lib/screens/finish/finish.dart
+++ App/connect_four/lib/screens/finish/finish.dart
@@ -1,6 +1,10 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:connect_four/components/rounded_button.dart';
 import 'package:connect_four/constants/constants.dart';
+import 'package:connect_four/game_controller/controller.dart';
+import 'package:connect_four/screens/game_screen/board_style.dart';
+import 'package:connect_four/screens/game_screen/cell.dart';
+import 'package:connect_four/screens/game_screen/column.dart';
 import 'package:connect_four/screens/welcome/welcome_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +13,16 @@ class Finish extends StatelessWidget {
   String message;
 
   Finish({this.message});
+
+  List<Widget> finalBoardBuilder() {
+    List<Widget> column = <Widget>[];
+    for (int j = 0; j < Constants.COLS; ++j) {
+      column.add(BoardColumn(
+        colNumber: j,
+      ));
+    }
+    return column;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +46,7 @@ class Finish extends StatelessWidget {
                 repeatForever: true,
               ),
             ),
+            BoardStyle(board: finalBoardBuilder()),
             RoundedButton(
               text: "PLAY AGAIN ",
               press: () {
